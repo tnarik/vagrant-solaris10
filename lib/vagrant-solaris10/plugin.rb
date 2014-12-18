688dd0a24dde0cb1c37e4454e84bec173941cce7
tnarik <tnarik@gmail.com>
2014-12-18 14:11:15 +0000

    Main plugin file has been splitted.

---
+++ lib/vagrant-solaris10/plugin.rb
@@ -0,0 +1,36 @@
+require "vagrant"
+
+module Vagrant
+  module Solaris10
+
+    class Plugin < Vagrant.plugin("2")
+      name "Solaris10 guest"
+      description "Solaris10 guest support."
+
+      guest_capability("solaris", "mount_vmware_shared_folder") do
+        require_relative "cap/mount_vmware_shared_folder"
+        Cap::MountVmwareSharedFolder 
+      end
+
+      guest_capability("solaris", "mount_parallels_shared_folder") do
+        require_relative "cap/mount_parallels_shared_folder"
+        Cap::MountParallelsSharedFolder 
+      end
+
+      guest_capability("solaris", "insert_public_key") do
+        require_relative "cap/insert_public_key"
+        Cap::InsertPublicKey
+      end
+
+      guest_capability("solaris", "remove_public_key") do
+        require_relative "cap/remove_public_key"
+        Cap::RemovePublicKey
+      end
+
+      guest_capability("solaris", "rsync_post") do
+        require_relative "cap/rsync"
+        Cap::RSync
+      end
+    end
+  end
+end
